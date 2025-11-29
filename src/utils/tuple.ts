bd7d5999a111a276c13cc988e21c678be5a07d76
eniallator <eniallatormc@gmail.com>
2025-11-29 11:42:24 +0000

    Configured/added some utils

---
+++ src/utils/tuple.ts
@@ -0,0 +1,13 @@
+export const tuple = <const T extends unknown[]>(...tuple: T) => tuple;
+
+type RecursionLimit = 1001;
+
+export type FillTuple<
+  T,
+  N extends number,
+  A extends T[] = [T]
+> = A["length"] extends RecursionLimit
+  ? [T, ...T[]]
+  : A["length"] extends N
+  ? A
+  : FillTuple<T, N, [...A, T]>;
